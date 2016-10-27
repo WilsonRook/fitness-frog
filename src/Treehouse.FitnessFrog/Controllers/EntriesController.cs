9530f5b5b4cde9cc29ad959c16ba2349aadaa0d3
Wilson <wilson.rook@gmail.com>
2016-10-27 13:07:59 -0400

    Updated the controller to support the "Delete" page

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -122,7 +122,26 @@ namespace Treehouse.FitnessFrog.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            return View();
+            // TODO Retrieve entry for the provided id parameter value.
+            Entry entry = _entriesRepository.GetEntry((int)id);
+
+            // TODO return 'Not Found' if an entry wasnt found
+            if (entry == null)
+            {
+                return HttpNotFound();
+            }
+            // TODO pass the entry to the view.
+            return View(entry);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            // TODO Delete the entry.
+            _entriesRepository.DeleteEntry(id);
+
+            // Redirect to the "Entries" list page
+            return RedirectToAction("Index");
         }
 
         private void ValidateEntry(Entry entry)
